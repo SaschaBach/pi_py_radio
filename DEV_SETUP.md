19982e40219c4299498a699f7d3c1d3b8918d9b3
SaschaBach <Sascha@saschas-mbp.speedport.ip>
2021-02-15 13:29:37 +0100

    bissl huebsch gemacht

--- DEV_SETUP.md
+++ DEV_SETUP.md
@@ -1,5 +1,21 @@
+# Fuer den Mac
+# Homebrew 
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# VLC
+brew install vlc
+# GIT
+brew install git
 # Um das Entwicklungsverzeichnis auf dem Pi innerhalb der lokalen Maschine zu mounten
 sshfs pi@192.168.2.114:/home/pi/Desktop/pi_py_radio ~/Desktop/pi_py_radio
 
+# Fuer den Pi
+# VLC
+brew install vlc
+# GIT
+brew install git
+
+# Fuer beide
 # Um VLC zu starten muss erst die python lib für VLC installiert werden
 pip install python-vlc
+
+
--- radiosenderSwitch.py
+++ radiosenderSwitch.py
@@ -3,12 +3,15 @@ from radiosender import Radiosender
 
 class RadiosenderSwitch(object):
 
+    hr3 = 'HR3'
+    radioBob = 'RadioBob'
+
     def __init__(self):
         self.myLogger = MyLogger(self.__class__.__name__)
         self.myLogger.info('Init RadioSwitch')
 
-        self.radiosenderDict = {'RadioBob':Radiosender('http://streams.radiobob.de/bob-live/mp3-192/mediaplayer','RadioBob'), 
-        'HR3':Radiosender('http://hr-hr3-live.cast.addradio.de/hr/hr3/live/mp3/128/stream.mp3','HR3')}
+        self.radiosenderDict = {self.radioBob:Radiosender('http://streams.radiobob.de/bob-live/mp3-192/mediaplayer',self.radioBob), 
+        self.hr3:Radiosender('http://hr-hr3-live.cast.addradio.de/hr/hr3/live/mp3/128/stream.mp3',self.hr3)}
 
     def get_radiosender(self, name):
         self.myLogger.info('get_radiosender(' + name + ')')
